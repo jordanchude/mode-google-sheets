128927a6b4d165a0233052ec173b982967370f7e
Jordan <jordan@chude.co>
2021-10-26 09:56:48 -0400

    formatted sql

--- report.sql
+++ report.sql
@@ -1,4 +1,9 @@
 -- Returns all number one records from specific year
-SELECT * FROM tutorial.billboard_top_100_year_end 
-WHERE year_rank = 1
-ORDER BY year_rank, year ASC;
+SELECT
+  *
+FROM
+  tutorial.billboard_top_100_year_end
+WHERE
+  year_rank = 1
+ORDER BY
+  year ASC;
